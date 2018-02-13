8a7166404e25db446d8484a6b9c94a393de85061
Mickael <mickael.guenot@infotel.com>
2018-02-13 16:29:25 +0100

    Premier commit pour tester

---
+++ src/com/formation/test/CTest.java
@@ -0,0 +1,10 @@
+package com.formation.test;
+
+public class CTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ceci est un test");
+	}
+
+}
